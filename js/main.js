8a460beca88fe4a22040a55849ef5a94e311a49a
yavuzhan <gtacinq45@gmail.com>
2023-09-23 17:17:20 +0200

    new

--- js/main.js
+++ js/main.js
@@ -29,3 +29,29 @@ $(document).ready(function () {
         });
     });
 });
+
+
+const inputEl = document.querySelector(".input");
+
+const bodyEl = document.querySelector("body");
+
+inputEl.checked = JSON.parse(localStorage.getItem("mode"));
+
+updateBody();
+
+function updateBody() {
+    if (inputEl.checked) {
+        bodyEl.style.background = "black";
+    } else {
+        bodyEl.style.background = "white";
+    }
+}
+
+inputEl.addEventListener("input", () => {
+    updateBody();
+    updateLocalStorage();
+});
+
+function updateLocalStorage() {
+    localStorage.setItem("mode", JSON.stringify(inputEl.checked));
+}
